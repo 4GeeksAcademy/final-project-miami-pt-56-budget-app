7185eeef25ad7a7ec2df442f67aa2a108ba57460
Christa Kelly <46685557+christadkelly@users.noreply.github.com>
2023-11-21 17:02:35 +0000

    Finished model

--- src/api/models.py
+++ src/api/models.py
@@ -2,29 +2,26 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-user_friend = db.Table(
-    "user_friend",
-    db.Column("user_id", db.ForeignKey("user.id")),
-    db.Column("friend_id", db.ForeignKey("friend.id")),
-)
+user_friend= db.Table('user_friend',
+    db.Column('user_1', db.Integer, db.ForeignKey('user.id')),
+    db.Column('user_2', db.Integer, db.ForeignKey('user.id')))
 
-user_group = db.Table(
-    "user_group",
-    db.Column("user_id", db.ForeignKey("user.id")),
-    db.Column("group_id", db.ForeignKey("group.id")),
-    db.Column("friend_id", db.ForeignKey("friend.id"))
-)
+group_member = db.Table('group_member',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('group_id', db.Integer, db.ForeignKey('group.id')))
 
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    first_name = db.Column(db.String(120), unique=True, nullable=False)
-    last_name = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-
-    friends = db.relationship('Friend', secondary=user_friend)
-    groups = db.relationship('Group', secondary=user_group)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    friends = db.relationship("User", secondary=user_friend, 
+                              primaryjoin=id == user_friend.c.user_1,
+                              secondaryjoin=id == user_friend.c.user_2, 
+                              back_populates="friends")
+    groups = db.relationship("Group", secondary=group_member, back_populates="members")
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -33,16 +30,18 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            "name": f'{self.first_name} {self.last_name}',
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "friends": list(map(lambda x: x.serialize(), self.friends)),
+            "groups": list(map(lambda x: x.serialize(), self.groups))
             # do not serialize the password, it's a security breach
         }
 
 class Group(db.Model):
     __tablename__ = 'group'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
-
-    members = db.relationship('Friend', secondary=user_group)
+    name = db.Column(db.String(120), nullable=False)
+    members = db.relationship("User", secondary=group_member, back_populates="groups")
 
     def __repr__(self):
         return f'<Group {self.name}>'
@@ -51,20 +50,7 @@ class Group(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            "members": [friend.serialize() for friend in self.members],
+            "members": list(map(lambda x: x.serialize(), self.members))
         }
+    
 
-class Friend(db.Model):
-    __tablename__ = 'friend'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return f'<Friend {self.name}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            # do not serialize the password, it's a security breach
-        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Group
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
@@ -25,19 +25,19 @@ def handle_hello():
 @api.route('/signup', methods= ['POST'])
 def handle_signup():
     email = request.json.get('email', None)
-    first_name = request.json.get('firstname', None)
-    last_name = request.json.get('lastname', None)
+    first_name = request.json.get('first_name', None)
+    last_name = request.json.get('last_name', None)
     password = request.json.get('password', None)
     verifypassword = request.json.get('verifypassword', None)
     newUser = User(email = email, password = password, first_name = first_name, last_name = last_name)
     if password != verifypassword:
-        return jsonify('Passwords do not match'), 400
+        return jsonify('Passwords do not match'), 406
     if User.query.filter_by(email = email).first() == None:
         db.session.add(newUser)
         db.session.commit()
         return jsonify('Added User'), 200
     else:
-        return jsonify('That email is already in use'), 400
+        return jsonify('That email is already in use'), 412
     
 @api.route('/login', methods=['POST'])
 def handle_login():
@@ -50,87 +50,142 @@ def handle_login():
         access_token = create_access_token(identity=user.id)
     return jsonify({"token": access_token, "user_id": user.id}), 200
 
-@api.route('/home', methods=['GET'])
-@jwt_required()
-def handle_home():
-    current_user_id = get_jwt_identity()
-    user = User.query.get(current_user_id)
-    if user is not None:
-        return jsonify({"user": user.serialize(), "status":"ok"}), 200
-    else:
-        return jsonify({'msg': 'You must be logged in'}), 400
+# @api.route('/home', methods=['GET'])
+# @jwt_required()
+# def handle_home():
+#     current_user_id = get_jwt_identity()
+#     user = User.query.get(current_user_id)
+#     if user is not None:
+#         return jsonify({"user": user.serialize(), "status":"ok"}), 200
+#     else:
+#         return jsonify({'msg': 'You must be logged in'}), 400
 
-@api.route('/groups', methods=['GET'])
-@jwt_required()
-def handle_get_groups():
-    current_user_id = get_jwt_identity()
-    user = User.query.get(current_user_id)
-    if user is not None:
-        return jsonify(user.serialize()['groups']), 200
-    else:
-        return jsonify({'msg': 'You must be logged in'}), 400
+# @api.route('/groups', methods=['GET'])
+# @jwt_required()
+# def handle_get_groups():
+#     current_user_id = get_jwt_identity()
+#     user = User.query.get(current_user_id)
+#     if user is not None:
+#         return jsonify(user.serialize()['groups']), 200
+#     else:
+#         return jsonify({'msg': 'You must be logged in'}), 400
 
-@api.route('/groups', methods=['POST'])
-@jwt_required()
-def handle_add_group():
-    current_user_id = get_jwt_identity()
-    user = User.query.get(current_user_id)
-    return jsonify({"email":user.email, 'groups': groups}), 200
+# @api.route('/groups', methods=['POST'])
+# @jwt_required()
+# def handle_add_group():
+#     current_user_id = get_jwt_identity()
+#     user = User.query.get(current_user_id)
+#     return jsonify({"email":user.email, 'groups': groups}), 200
 
-@api.route('/groups', methods=['DELETE'])
-@jwt_required()
-def handle_delete_group():
-    current_user_id = get_jwt_identity()
-    user = User.query.get(current_user_id)
-    if user is not None:
-        groupList = Group.query.all()
-        toDelete = None
-        for item in groupList:
-            if item.serialize()['name'] == name:
-                toDelete = item
-        if toDelete == None:
-            return jsonify("invalid group id"), 400
-        else:
-            db.session.delete(toDelete)
-            db.session.commit()
-            return jsonify("group deleted"), 200
-    else:
-        return jsonify({'msg': 'You must be logged in'}), 400    
+# @api.route('/groups', methods=['DELETE'])
+# @jwt_required()
+# def handle_delete_group():
+#     current_user_id = get_jwt_identity()
+#     user = User.query.get(current_user_id)
+#     if user is not None:
+#         groupList = Group.query.all()
+#         toDelete = None
+#         for item in groupList:
+#             if item.serialize()['name'] == name:
+#                 toDelete = item
+#         if toDelete == None:
+#             return jsonify("invalid group id"), 400
+#         else:
+#             db.session.delete(toDelete)
+#             db.session.commit()
+#             return jsonify("group deleted"), 200
+#     else:
+#         return jsonify({'msg': 'You must be logged in'}), 400    
 
-@api.route('/friends', methods=['GET'])
-@jwt_required()
-def handle_get_friends():
-    current_user_id = get_jwt_identity()
-    user = User.query.get(current_user_id)
-    if user is not None:
-        return jsonify(user.serialize()['friends']), 200
-    else:
-        return jsonify({'msg': 'You must be logged in'}), 400
+# @api.route('/friends', methods=['GET'])
+# @jwt_required()
+# def handle_get_friends():
+#     current_user_id = get_jwt_identity()
+#     user = User.query.get(current_user_id)
+#     if user is not None:
+#         return jsonify(user.serialize()['friends']), 200
+#     else:
+#         return jsonify({'msg': 'You must be logged in'}), 400
 
-@api.route('/friends', methods=['POST'])
-@jwt_required()
-def handle_add_friend():
-    current_user_id = get_jwt_identity()
-    user = User.query.get(current_user_id)
-    return jsonify({"email":user.email, 'groups': groups}), 200
+# @api.route('/friends', methods=['POST'])
+# @jwt_required()
+# def handle_add_friend():
+#     current_user_id = get_jwt_identity()
+#     user = User.query.get(current_user_id)
+#     return jsonify({"email":user.email, 'groups': groups}), 200
 
-@api.route('/friends', methods=['DELETE'])
-@jwt_required()
-def handle_delete_friend():
-    current_user_id = get_jwt_identity()
-    user = User.query.get(current_user_id)
-    if user is not None:
-        friendList = Friend.query.all()
-        toDelete = None
-        for item in friendList:
-            if item.serialize()['name'] == name:
-                toDelete = item
-        if toDelete == None:
-            return jsonify("invalid friend id"), 400
-        else:
-            db.session.delete(toDelete)
-            db.session.commit()
-            return jsonify("friend deleted"), 200
-    else:
-        return jsonify({'msg': 'You must be logged in'}), 400  
+# @api.route('/friends', methods=['DELETE'])
+# @jwt_required()
+# def handle_delete_friend():
+#     current_user_id = get_jwt_identity()
+#     user = User.query.get(current_user_id)
+#     if user is not None:
+#         friendList = Friend.query.all()
+#         toDelete = None
+#         for item in friendList:
+#             if item.serialize()['name'] == name:
+#                 toDelete = item
+#         if toDelete == None:
+#             return jsonify("invalid friend id"), 400
+#         else:
+#             db.session.delete(toDelete)
+#             db.session.commit()
+#             return jsonify("friend deleted"), 200
+#     else:
+#         return jsonify({'msg': 'You must be logged in'}), 400  
+
+@api.route('/user/<int:user_id>/friends', methods=['PUT', 'DELETE'])
+def handle_add_friends(user_id):
+    body = request.get_json()
+
+    # Add Friends
+    if request.method == 'PUT':
+        user = User.query.get(user_id)
+
+        try:
+            if user.friends is None:
+                user.friends = []
+            new_friend = User.query.filter_by(body["new_friend_id"]).first()
+            user.friends.append(new_friend)
+        
+        except Exception as e:
+            payload = {
+                'msg': "Couldn't add friend. Try again later.",
+                'error': e
+            }
+            return jsonify(payload), 409
+
+        db.session.commit()
+
+        user = User.query.get(user_id)
+
+        payload = {
+            'msg': "Successfully added friend",
+            'user': user.serialize()
+        }
+        return jsonify(payload), 200
+
+
+    # Delete Friend
+    if request.method == 'DELETE':
+        user = User.query.get(user_id)
+        friend = User.query.filter_by(body["old_friend"])
+
+        user.friends.remove(friend)
+        db.session.commit()
+
+        payload = {
+            'msg': "Successfully removed friend",
+            'user': user.serialize()
+        }
+        return jsonify(payload), 200
+
+
+@api.route('/bookmark/user/<user_id>', methods=['GET'])
+def get_all_bookmarks(user_id):
+
+    user = User.query.get(user_id)
+
+    serialized_bookmarks = [item.serialize() for item in user.bookmarks]
+    return jsonify(serialized_bookmarks), 200
 
