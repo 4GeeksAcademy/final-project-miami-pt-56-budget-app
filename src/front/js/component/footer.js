34a93b2bc6c4a30eb258146e125e3c39e716a914
Valeria V <54474237+lvvargas-aponte@users.noreply.github.com>
2023-11-28 16:09:24 +0000

    fixed footer to bottom of VP

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer mt-auto py-3 text-center fixed-bottom">
 		<p>&copy; {new Date().getFullYear()} BetterBudget </p>
 	</footer>
 );
