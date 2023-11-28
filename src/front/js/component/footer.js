29124c7931102ee07b513dfec96a45cb6a5a2ff1
Valeria V <54474237+lvvargas-aponte@users.noreply.github.com>
2023-11-28 18:24:52 +0000

    updated footer to stick to bottom without overlapping on main content

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center fixed-bottom">
-		<p>&copy; {new Date().getFullYear()} BetterBudget </p>
+	<footer className="footer mt-auto py-1 text-center">
+		<div className="container">
+		<p className="m-1">&copy; {new Date().getFullYear()} BetterBudget </p>
+		</div>
 	</footer>
 );
