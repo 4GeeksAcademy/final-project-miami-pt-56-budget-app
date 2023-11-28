7487fbbeec45cc2b9147cb8d7f76e1e3055f4db3
Valeria V <54474237+lvvargas-aponte@users.noreply.github.com>
2023-11-28 00:25:38 +0000

    footer fixed

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
 		<p>&copy; {new Date().getFullYear()} BetterBudget </p>
 	</footer>
 );
