af60e057c3095b9c622ebc9c151be195f426859d
Benjamin Ly <127355682+nebly2@users.noreply.github.com>
2023-12-06 23:11:50 +0000

    wip

--- src/front/js/component/FriendsTable.js
+++ src/front/js/component/FriendsTable.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Container, Table } from "react-bootstrap";
