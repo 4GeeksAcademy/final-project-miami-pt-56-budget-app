01725edfdb45d3d004715a25c882192656b589c3
Valeria Vargas <54474237+lvvargas-aponte@users.noreply.github.com>
2023-12-10 14:57:13 +0000

    added favicon

--- webpack.common.js
+++ webpack.common.js
@@ -40,7 +40,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        favicon: '4geeks.ico',
+        favicon: 'better-budget-favicon.ico',
         template: 'template.html'
     }),
     new Dotenv({ safe: true, systemvars: true })
